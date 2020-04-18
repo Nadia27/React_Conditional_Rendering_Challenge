3020fabcf49b6486f6f5017b428e3b77d7db8a6e
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-18 16:08:02 -0400

    Conditional Render
    Change form based on if user is registered using ternary/ && operators

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Input from "./Input";
 
-function Form() {
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+function Form(props) {
   return (
     <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      {!props.isRegistered && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
